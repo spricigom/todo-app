1f2e2afd36c19b233ff0293737803befa612686b
spricigom <emilispricigo.ifc@gmail.com>
2024-11-08 16:45:01 -0300

    reactve native

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet,Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Oi.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    
+    }
+})
